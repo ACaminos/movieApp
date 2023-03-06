11acb88ec3a78197e764497a52d90bf0cf1352f6
Ariel Caminos <caminosariel@gmail.com>
2023-03-06 15:41:46 -0300

    edit

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'ef63bf611408077ac851eed1be11c60b',
+        api_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
         languaje: 'es-ES'
     }
 })
