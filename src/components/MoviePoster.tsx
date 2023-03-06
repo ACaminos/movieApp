5c0c2b4efa1fdc8de591b5236e6013e212a1ebaa
Ariel <caminosariel@gmail.com>
2023-03-06 14:45:24 -0300

    moviePoster show posters

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, Image, StyleSheet} from 'react-native'
+import { Movie } from '../interfaces/movieInterfaces';
 
-export const MoviePoster = () => {
+interface Props {
+  movie: Movie;
+}
+
+export const MoviePoster = ( { movie }: Props ) => {
+
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+
   return (
-    <View>
-        <Text>Movie Poster</Text>
+    <View style={{
+      width: 300,
+      height: 420,
+    }}>
+      <View style={styles.imageContainer}>
+        <Image source={{  uri }} style={styles.image} />
+      </View>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  image: {
+    flex:1,
+    borderRadius: 18,
+    },
+  imageContainer: {
+    flex: 1,
+    borderRadius:18,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+    elevation:10,
+  }
+});
